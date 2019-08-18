ca9c65a2d0997bfef7eec84641fee3dee1b804d9
Sphini <spdop2000@gmail.com>
2019-08-19 01:48:46 +0500

    Внес правки предзащиты

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task("html", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
     "source/*.ico",
